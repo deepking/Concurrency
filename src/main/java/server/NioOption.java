9709282ee76bf155a7aaf277ca2343134a0c1218
chengyi <t0963162429@gmail.com>
2012-09-10 21:36:00 +0800

    init

---
+++ src/main/java/server/NioOption.java
@@ -0,0 +1,45 @@
+package server;
+
+
+public enum NioOption
+{
+	//------------------------------------------------
+	// channel config
+	bufferFactory,  // Returns the default ChannelBufferFactory used to create a new ChannelBuffer.
+	pipelineFactory, // Returns the ChannelPipelineFactory which be used when a child channel is created.
+    connectTimeoutMillis, // connect timeout of the channel in milliseconds.
+	//------------------------------------------------
+    keepAlive, // SO_KEEPALIVE option, Turn on socket keepalive. Valid for Socket.
+    reuseAddress,	// SO_REUSEADDR, Enable reuse address for a socket. Valid for Socket, ServerSocket, DatagramSocket.
+    soLinger,	// Specify a linger-on-close timeout, Valid for Sockets
+    tcpNoDelay, // SO_TCPNODELAY option, Valid for Sockets
+    trafficClass, // traffic class
+    receiveBufferSize, // SO_RCVBUF 
+    				   // Get the size of the buffer actually used by the platform when receiving in data on this socket.
+    				   // Valid for all sockets: Socket, ServerSocket, DatagramSocket. 
+    sendBufferSize, // SO_SNDBUF, Set a hint the size of the underlying buffers for outgoing network I/O. Valid for all sockets: Socket, ServerSocket, DatagramSocket.
+    // SO_TIMEOUT: Specify a timeout on blocking socket operations. (Don't block forever! Valid for all sockets: Socket, ServerSocket, DatagramSocket.
+    // SO_BROADCAST: Enables a socket to send broadcast messages. Valid for DatagramSocket.
+    // SO_OOBINLINE: Enable inline reception of TCP urgent data. Valid for Socket.
+    // IP_MULTICAST_IF: Specify the outgoing interface for multicast packets (on multihomed hosts). Valid for MulticastSockets.
+    // IP_MULTICAST_LOOP: Enables or disables local loopback of multicast datagrams. Valid for MulticastSocket.
+    // IP_TOS: Sets the type-of-service or traffic class field in the IP header for a TCP or UDP socket. Valid for Socket, DatagramSocket
+    // setPerformancePreferences(int connectionTime, int latency, int bandwidth) 
+	writeBufferHighWaterMark,
+	writeBufferLowWaterMark,
+	writeSpinCount,
+	
+	backlog, // server socket 使用
+	//------------------------------------------------
+    child_tcpNoDelay,
+    child_keepAlive,
+    ;
+    //------------------------------------------------
+    public String getOptionName()
+    {
+    	String sName = name();
+    	if (sName.startsWith("child_"))
+    		sName = "child." + sName.substring(6);
+    	return sName;
+    }
+}
---
+++ src/main/java/server/NioServer.java
@@ -0,0 +1,84 @@
+package server;
+
+import static server.NioOption.child_keepAlive;
+import static server.NioOption.child_tcpNoDelay;
+import static server.NioOption.reuseAddress;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NioServer
+{
+	private static NioServerSocketChannelFactory sm_channelFactory = new NioServerSocketChannelFactory(
+	        Executors.newCachedThreadPool(), Executors.newCachedThreadPool());
+	private static final Logger log = LoggerFactory.getLogger(NioServer.class);
+
+	private String m_sLocalIP;
+	private int m_nLocalPort;
+	private ServerBootstrap m_bootstrap;
+
+	//------------------------------------------------------------------------
+	public NioServer(String sLocalIP,
+	        int nLocalPort, boolean bChildTCPDelay, boolean bChildKeepAlive,
+	        boolean bReuseAddress)
+	{
+		// 1. create ServerFactory and ServerBootstrap
+		m_bootstrap = new ServerBootstrap(sm_channelFactory);
+
+		// 2. set options
+		setOption(child_tcpNoDelay, !bChildTCPDelay);
+		setOption(child_keepAlive, bChildKeepAlive);
+
+		// 3. reuse server's side address
+		setOption(reuseAddress, bReuseAddress);
+
+		// 4. set pipeline
+		//m_bootstrap.setPipelineFactory(pipelineFactory);
+
+		// 5. bind 移到 start()，以便讓 start() 可以有機會運作
+		m_sLocalIP = sLocalIP;
+		m_nLocalPort = nLocalPort;
+	}
+
+	public NioServer(int nLocalPort)
+	{
+		this(null, nLocalPort, false, false, true);
+	}
+
+	// -----------------------------------------------------------------------------
+	public void start()
+	{
+		log.debug("start server port=" + m_nLocalPort);
+
+		// 5. bind
+		if (m_sLocalIP == null)
+			m_bootstrap.bind(new InetSocketAddress(m_nLocalPort));
+		else
+			m_bootstrap.bind(new InetSocketAddress(m_sLocalIP, m_nLocalPort));
+	}
+
+	public void stop()
+	{
+		log.debug("stop server port=" + m_nLocalPort);
+		
+		// 如果和其他 protocol 共用，會關到其他 protocol
+		sm_channelFactory.releaseExternalResources();
+	}
+
+	public void setOption(NioOption op, Object value)
+	{
+		m_bootstrap.setOption(op.getOptionName(), value);
+	}
+
+	public void setPipelineFactory(ChannelPipelineFactory pipelineFactory)
+    {
+	    m_bootstrap.setPipelineFactory(pipelineFactory);
+    }
+
+}
