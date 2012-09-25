26085c9b579f243bca5973a0715c66316e67b87d
chengyi <t0963162429@gmail.com>
2012-09-26 01:24:04 +0800

    confg worker count

--- src/main/java/server/BroadcastServer.java
+++ src/main/java/server/BroadcastServer.java
@@ -12,6 +12,7 @@ import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFactory;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
@@ -37,143 +38,168 @@ import com.google.common.util.concurrent.Uninterruptibles;
 
 public class BroadcastServer
 {
-	private static final Logger log = LoggerFactory.getLogger(BroadcastServer.class);
-	
-	// handler
-	//
-	private final Recipients m_handler = new Recipients();
-	private final LengthFieldPrepender m_lenPrePender = new LengthFieldPrepender(4);
-	private final AtomicInteger m_count = new AtomicInteger(1);
-	
-	public void run(int nPort)
-	{
-		ServerBootstrap bootstrap = new ServerBootstrap();
-		bootstrap.setOption(NioOption.child_tcpNoDelay.toString(), true);
-		bootstrap.setFactory(new NioServerSocketChannelFactory());
-		bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
-			public ChannelPipeline getPipeline() throws Exception
-			{
-				return Channels.pipeline(m_lenPrePender, m_handler);
-			}
-		});
-		
-		System.out.println(bootstrap);
-		Channel channel = bootstrap.bind(new InetSocketAddress(nPort));
-		log.info("start server port={} channel={}", nPort, channel);
-		log.info("default thread={}", Runtime.getRuntime().availableProcessors() * 2);
-	}
-	
-	public ChannelGroupFuture write(Object message)
+    private static final Logger log = LoggerFactory
+            .getLogger(BroadcastServer.class);
+
+    // handler
+    //
+    private final Recipients m_handler = new Recipients();
+    private final LengthFieldPrepender m_lenPrePender = new LengthFieldPrepender(
+            4);
+    private final AtomicInteger m_count = new AtomicInteger(1);
+
+    private final ChannelFactory m_factory;
+
+    public BroadcastServer(ChannelFactory factory)
     {
-	    final Stopwatch sw = new Stopwatch().start();
-	    ChannelGroupFuture f = m_handler.write(message);
-	    f.addListener(new ChannelGroupFutureListener()
+        m_factory = factory;
+    }
+
+    public void run(int nPort)
+    {
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.setOption(NioOption.child_tcpNoDelay.toString(), true);
+        bootstrap.setFactory(m_factory);
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory()
         {
-            @Override
-            public void operationComplete(ChannelGroupFuture future) throws Exception
+            public ChannelPipeline getPipeline() throws Exception
             {
-                long lMillis = sw.elapsedMillis();
-                log.debug("writeComplete {} ms, {}", lMillis, m_count.getAndIncrement());
+                return Channels.pipeline(m_lenPrePender, m_handler);
             }
         });
-	    
-	    return f;
+
+        System.out.println(bootstrap);
+        Channel channel = bootstrap.bind(new InetSocketAddress(nPort));
+        log.info("start server port={} channel={}", nPort, channel);
+        log.info("default thread={}", Runtime.getRuntime()
+                .availableProcessors() * 2);
     }
-	
-	//------------------------------------------------------------------------
-	static class Recipients extends SimpleChannelUpstreamHandler
-	{
-		ChannelGroup m_recipients = new DefaultChannelGroup();
-		ObjectSizeEstimator estimator = new DefaultObjectSizeEstimator();
-		
-		@Override
-		public void channelInterestChanged(ChannelHandlerContext ctx,
-				ChannelStateEvent e) throws Exception {
-			
-			log.info("channel State={} {}", e, ctx.getChannel());
-		}
-		
-		@Override
-		public void channelOpen(ChannelHandlerContext ctx, ChannelStateEvent e)
-		        throws Exception
-		{
-		    synchronized (m_recipients)
-            {
-    			m_recipients.add(e.getChannel());
-            }
-		}
-		
-		@Override
-		public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e)
-		        throws Exception
-		{
-		    synchronized (m_recipients)
-            {
-    			m_recipients.remove(e.getChannel());
-            }
-		    log.debug("[Close] {}", e.getChannel());
-		}
-		
-		@Override
-		public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e)
-		        throws Exception
-		{
-		    log.warn("exception", e.getCause());
-		}
-		
-		public ChannelGroupFuture write(Object message)
-        {
-//		    log.debug("send {} bytes to {} clients", estimator.estimateSize(message), 
-//		    		m_recipients.size());
-		    
-	        return m_recipients.write(message);
-        }
-	}
-	
-	//------------------------------------------------------------------------
-	public static void main(String[] args)
+
+    public ChannelGroupFuture write(Object message)
     {
-	    //int nPort = Integer.parseInt(args[0]);
-	    final Param param = new Param();
-	    JCommander jcommander = new JCommander(param);
-	    jcommander.setProgramName("server");
-	    jcommander.usage();
-	    jcommander.parse(args);
-	    
-		final BroadcastServer server = new BroadcastServer();
-		server.run(param.port);
-		
-		final byte[] bytes = new byte[param.sendByteSize];
-		
-		ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-		service.scheduleAtFixedRate(new Runnable()
-		{
-			public void run()
-			{
-				ChannelBuffer buf = ChannelBuffers.buffer(8 + param.sendByteSize);
-				buf.writeLong(System.currentTimeMillis());
-				buf.writeBytes(bytes);
-				
-				server.write(buf);
-			}
-		}, param.sendPeriodMillis, param.sendPeriodMillis, TimeUnit.MILLISECONDS);
-		
-		Uninterruptibles.joinUninterruptibly(Thread.currentThread());
+        final Stopwatch sw = new Stopwatch().start();
+        ChannelGroupFuture f = m_handler.write(message);
+        f.addListener(new ChannelGroupFutureListener()
+        {
+            @Override
+            public void operationComplete(ChannelGroupFuture future)
+                    throws Exception
+            {
+                long lMillis = sw.elapsedMillis();
+                log.debug("writeComplete {} ms, {}", lMillis,
+                        m_count.getAndIncrement());
+            }
+        });
+
+        return f;
     }
-	
-	static class Param 
-	{
-	    @Parameter(names="-port")
-	    private int port = 9999;
-	    
-	    @Parameter(names="-sendPeriodMillis")
-	    private int sendPeriodMillis = 100;
-	    
-	    @Parameter(names="-sendByteSize")
-	    private int sendByteSize = 100;
-	    
-	    @Parameter(names="-sendBufSize")
-	    private int sendBufSize = 100;
-	    
-	}
+
+    // ------------------------------------------------------------------------
+    static class Recipients extends SimpleChannelUpstreamHandler
+    {
+        ChannelGroup m_recipients = new DefaultChannelGroup();
+        ObjectSizeEstimator estimator = new DefaultObjectSizeEstimator();
+
+        @Override
+        public void channelInterestChanged(ChannelHandlerContext ctx,
+                ChannelStateEvent e) throws Exception
+        {
+
+            log.info("channel State={} {}", e, ctx.getChannel());
+        }
+
+        @Override
+        public void channelOpen(ChannelHandlerContext ctx, ChannelStateEvent e)
+                throws Exception
+        {
+            synchronized (m_recipients)
+            {
+                m_recipients.add(e.getChannel());
+            }
+        }
+
+        @Override
+        public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e)
+                throws Exception
+        {
+            synchronized (m_recipients)
+            {
+                m_recipients.remove(e.getChannel());
+            }
+            log.debug("[Close] {}", e.getChannel());
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e)
+                throws Exception
+        {
+            log.warn("exception", e.getCause());
+        }
+
+        public ChannelGroupFuture write(Object message)
+        {
+            // log.debug("send {} bytes to {} clients",
+            // estimator.estimateSize(message),
+            // m_recipients.size());
+
+            return m_recipients.write(message);
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    public static void main(String[] args)
+    {
+        // int nPort = Integer.parseInt(args[0]);
+        final Param param = new Param();
+        JCommander jcommander = new JCommander(param);
+        jcommander.setProgramName("server");
+        jcommander.usage();
+        jcommander.parse(args);
+
+        final BroadcastServer server = new BroadcastServer(
+                new NioServerSocketChannelFactory(
+                        Executors.newCachedThreadPool(),
+                        Executors.newCachedThreadPool(), param.workerCount));
+        server.run(param.port);
+
+        final byte[] bytes = new byte[param.sendByteSize];
+
+        ScheduledExecutorService service = Executors
+                .newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(
+                new Runnable()
+                {
+                    public void run()
+                    {
+                        ChannelBuffer buf = ChannelBuffers
+                                .buffer(8 + param.sendByteSize);
+                        buf.writeLong(System.currentTimeMillis());
+                        buf.writeBytes(bytes);
+
+                        server.write(buf);
+                    }
+                }, param.sendPeriodMillis, param.sendPeriodMillis,
+                TimeUnit.MILLISECONDS);
+
+        Uninterruptibles.joinUninterruptibly(Thread.currentThread());
+    }
+
+    static class Param
+    {
+        @Parameter(names = "-port")
+        private int port = 9999;
+
+        @Parameter(names = "-sendPeriodMillis")
+        private int sendPeriodMillis = 100;
+
+        @Parameter(names = "-sendByteSize")
+        private int sendByteSize = 100;
+
+        @Parameter(names = "-sendBufSize")
+        private int sendBufSize = 100;
+
+        @Parameter(names = "-workerCount")
+        private int workerCount = 32;
+
+    }
 }
